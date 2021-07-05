1322d094bb36047033fbde25616d46043ad320c6
robgen <robertogentile90@gmail.com>
2021-07-05 16:14:45 +0200

    micro modifications

--- exampleLBD.m
+++ exampleLBD.m
@@ -1,9 +1,13 @@
+%% Load stuff
+
 GPfolder = '/Users/roberto/Dropbox/2. research/2021 CDR to risk/B_fittingGP/surrogatedPSDM';
 addpath('/Users/roberto/Dropbox/2. research/2021 CDR to risk/B_fittingGP/surrogatedPSDM')
 load(fullfile(GPfolder, 'fullFit.mat'))
 
+%% Part 1: SDoF target design
+
 EALtarget = 0.5;
-designSDoF = 147; % update after using the slider
+designSDoF = 117; % update after using the slider
 
 design = lossBasedDesign;
 design = getSeedEAL(design, fullFit);
@@ -13,3 +17,12 @@ plotEAL(design);
 
 design = getFrameMemberDetailing(design);
 plotFrameMemberDetailing(design)
+
+%% Part 2: design members
+
+% design the frame members to match the requirements of DDBD
+% create a slamasolver file
+% create the ruaumoko model from SLaMAsolver
+
+%% Part 3: 
+
